3ed697bcdc1632fac783b0ec1c53881c89833d6f
Kasper Helsted <khelst13@student.aau.dk>
2019-04-01 13:24:06 +0200

    Init vision

---
+++ DroneVision/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+
+namespace DroneVision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoCapture videoCapture = new VideoCapture(@"./bebop.sdp");
+
+            var frame = videoCapture.QueryFrame();
+
+            while (frame != null)
+            {
+                CvInvoke.Imshow("frame", frame);
+                CvInvoke.WaitKey(1);
+                frame = videoCapture.QueryFrame();
+            }
+        }
+    }
+}
