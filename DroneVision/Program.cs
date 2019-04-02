e888fc64190281b65ebe5bd18e2bf7d0614800a6
Kasper Helsted <khelst13@student.aau.dk>
2019-04-02 13:28:05 +0200

    Added test for streambuffer

--- DroneVision/Program.cs
+++ DroneVision/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Emgu.CV;
+using VidBuffLib;
 
 namespace DroneVision
 {
@@ -12,21 +14,24 @@ namespace DroneVision
     {
         static void Main(string[] args)
         {
-            Environment.SetEnvironmentVariable("OPENCV_FFMPEG_CAPTURE_OPTIONS", "protocol_whitelist;file,rtp,udp", EnvironmentVariableTarget.User);
+            int width = 640, height = 480;
+
             VideoCapture videoCapture = new VideoCapture(@"./bebop.sdp");
+            StreamBuffer streamBuffer = new StreamBuffer(videoCapture, width, height);
 
-            var frame = videoCapture.QueryFrame();
+            streamBuffer.Start();
 
-            while (frame != null)
+            while (streamBuffer.IsRunning)
             {
-                using (frame)
+                var frame = streamBuffer.PopLastFrame();
+                if (frame != null)
                 {
                     CvInvoke.Imshow("frame", frame);
                     CvInvoke.WaitKey(1);
                 }
-
-                frame = videoCapture.QueryFrame();
             }
+
+            Console.ReadLine();
         }
     }
 }
